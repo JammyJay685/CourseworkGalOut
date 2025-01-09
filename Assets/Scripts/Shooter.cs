0accee05a9b3cf16f08f06fac8b6b91cd5c6e28e
JammyJay685 <jamiebrown685@googlemail.com>
2025-01-09 00:45:16 +0000

    SpaceOutlaws0.4
    The moons are now able to be shot and destroyed and the player can shoot

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    // The variables that control the weapons
+    public float range = 100f;
+    public float damage = 10f;
+    public float fireRate = 15f;
+    public Camera fpsCam;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    void Shoot(){
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hitInfo, range)){
+            Debug.Log("Hit: " + hitInfo.transform.name);
+            Target target = hitInfo.transform.GetComponent<Target>();
+            if (target!= null){
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
